aa428e94cb8d86a16e0e5437bb5ce9aab6d35f36
RouDePH <sergeygaras2002@gmail.com>
2023-09-06 16:42:01 +0300

    LW_5

---
+++ LW_5/src/main/java/com/ontu/LW_5/Lw5Application.java
@@ -0,0 +1,13 @@
+package com.ontu.LW_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lw5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lw5Application.class, args);
+	}
+
+}
---
+++ LW_5/src/test/java/com/ontu/LW_5/Lw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontu.LW_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lw5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
